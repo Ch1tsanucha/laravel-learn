661ab18bdf7b1046a2f0deac6bf2d368988b4c31
BaBoon <chitsanucha.onri@gmail.com>
2025-02-09 23:47:07 +0700

    init

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+<html lang="en">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Ninja network</title>
+</head>
+
+<body>
+    <h1>
+        Welcom to ninja
+    </h1>
+    <p>
+        click to find ninja
+    </p>
+    <a href="" class="btn">
+        Find ninja
+    </a>
+</body>
+
+</html>
