240fc5b2716eb42022e1a2fc706632322bde7d5d
Ch1tsanucha <chitsanucha.onri@gmail.com>
2025-02-20 21:56:00 +0700

    learn lazy load

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -4,11 +4,15 @@
     </x-slot:heading>
 
     <body>
+        <div class="grid grid-cols-2">
         @foreach ($jobs as $job)
+        <div class="p-3 mb-4 rounded-sm border-4 border-grey-500 px-10 w-10/12">
         <a href="/jobs/{{$job['id']}}">
-            <p>
-                <strong>Title {{$job['title']}} </strong> Salary : {{$job['salary']}}
+            <p class="text-blue-500">
+                <strong>Title {{$job['title']}} </strong> 
             </p>
+         
+
             <p>{{ $job->employer->name }} is 
                 @foreach ( $job->employer->job as $jj)
                 {{ $jj->title }} 
@@ -17,8 +21,14 @@
           
            
         </p>
+        <p>
+            Salary : {{$job['salary']}}
+            </p>
         </a>
+        </div>
         @endforeach
+        </div>
+        
 
     </body>
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,9 @@ Route::get('/', function () {
 });
 
 Route::get('/jobs', function () {
-
+$jobs = jobs::with('employer')->get();
     return view('jobs', [
-        'jobs' => jobs::all()
+        'jobs' => $jobs
     ]);
 });
 
